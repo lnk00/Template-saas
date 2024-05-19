b5dddc5adcc3db32eb8cb356d525663e4121116e
Damien Dumontet <damien.dumontet@protonmail.com>
2024-05-19 20:31:30 -0300

    fix colors

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,10 +19,8 @@ export default function Login() {
       <DotBackground />
       <Container className="h-screen flex items-center justify-center">
         <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-          <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-            Welcome to Vecko
-          </h2>
-          <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+          <h2 className="font-bold text-xl">Welcome to Vecko</h2>
+          <p className="text-slate-600 text-sm max-w-sm mt-2 dark:text-slate-300">
             Login to vecko to plan your week/month in the most proeficient ans
             simple way
           </p>
@@ -38,25 +36,25 @@ export default function Login() {
               <ArrowRight className="h-4" />
             </Button>
 
-            <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+            <div className="bg-gradient-to-r from-transparent via-slate-300 dark:via-slate-700 to-transparent my-8 h-[1px] w-full" />
 
             <div className="flex flex-col space-y-4">
               <button
-                className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full rounded-md h-10 font-medium shadow-input bg-slate-50 dark:bg-slate-900 dark:shadow-[0px_0px_1px_1px_var(--slate-800)]"
                 type="submit"
               >
-                <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                <IconBrandGithub className="h-4 w-4 text-slate-800 dark:text-slate-300" />
+                <span className="text-slate-700 dark:text-slate-300 text-sm">
                   GitHub
                 </span>
                 <BottomGradient />
               </button>
               <button
-                className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+                className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full rounded-md h-10 font-medium shadow-input bg-slate-50 dark:bg-slate-900 dark:shadow-[0px_0px_1px_1px_var(--slate-800)]"
                 type="submit"
               >
-                <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-                <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+                <IconBrandGoogle className="h-4 w-4 text-slate-800 dark:text-slate-300" />
+                <span className="text-slate-700 dark:text-slate-300 text-sm">
                   Google
                 </span>
                 <BottomGradient />
