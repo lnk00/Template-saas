15d3efcb378275e7c80514831d61a6157e99c871
Damien Dumontet <damien.dumontet@protonmail.com>
2024-05-19 20:24:29 -0300

    fix colors

--- components/header-block.tsx
+++ components/header-block.tsx
@@ -7,7 +7,7 @@ export function HeaderBlock() {
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-slate-300">
           2024
         </h1>
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-slate-800">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
           Week 20 : May 13 - May 19
         </h1>
       </div>
